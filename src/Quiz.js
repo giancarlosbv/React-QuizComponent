2cae76417a171283091921b6fda54b18819b0d30
Giancarlos Blanco <giancarlos.blanco@jpmchase.com>
2022-07-12 15:23:30 -0400

    quiz question component created

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import QuizQuestion from './QuizQuestion.js';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component{
@@ -7,17 +7,17 @@ class Quiz extends Component{
   constructor(props) {
     super(props);
     this.state = {quiz_position:1}
-  }
+  };
+
+
 
 
 
   render() {
+    // console.log(quizData.quiz_questions[this.state.quiz_position-1]);
+
     return (
-      <div>
-        <div className = "QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
-        </div>
-      </div>
+      <QuizQuestion quiz_question ={quizData.quiz_questions[this.state.quiz_position-1]} />
     )
   }
 }
